b0f7fd9b726d1c2590926dc5d1604bba3ac59be6
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2025-02-15 19:46:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,17 @@ resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "eks_subnet" {
+resource "aws_subnet" "eks_subnet_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "eks_subnet_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -35,14 +42,20 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet.id]
+    subnet_ids = [
+      aws_subnet.eks_subnet_1.id,
+      aws_subnet.eks_subnet_2.id
+    ]
   }
 }
 
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_role_arn   = aws_iam_role.eks_role.arn
-  subnet_ids      = [aws_subnet.eks_subnet.id]
+  subnet_ids      = [
+    aws_subnet.eks_subnet_1.id,
+    aws_subnet.eks_subnet_2.id
+  ]
   instance_types  = ["t3.medium"]
   scaling_config {
     desired_size = 2
